36f836c352f64bb87ca98420e38af1804045ffb7
EJW Laptop <git.laptop@ejwaller.com>
2022-03-06 23:24:09 +0000

    laptop

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -32,6 +32,7 @@ import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformatio
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -62,6 +63,7 @@ ClassicEditor.builtinPlugins = [
 	Table,
 	TableToolbar,
 	TextTransformation,
+	ImageResize,
 ];
 
 // Editor configuration.
