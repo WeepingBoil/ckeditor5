5b2b4a65b04062b82fc4953b23bea6a77cf1cbba
EJW Laptop <git.laptop@ejwaller.com>
2022-09-17 13:20:11 +0100

    GeneralHtmlSupport

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,30 +10,25 @@
 module.exports = {
 	extends: 'ckeditor5',
 	rules: {
+		'linebreak-style': 'off',
+		'comma-dangle': 'off',
+		'array-bracket-spacing': 'off',
 		'ckeditor5-rules/ckeditor-imports': 'error',
-		'ckeditor5-rules/license-header': [ 'error', {
-			headerLines: [
-				'/**',
-				' * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.',
-				' * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license',
-				' */'
-			]
-		} ]
 	},
 	overrides: [
 		{
-			files: [ '**/tests/**/*.js' ],
+			files: ['**/tests/**/*.js'],
 			rules: {
 				'no-unused-expressions': 'off',
 				'ckeditor5-rules/ckeditor-imports': 'off',
-				'ckeditor5-rules/no-cross-package-imports': 'off'
-			}
+				'ckeditor5-rules/no-cross-package-imports': 'off',
+			},
 		},
 		{
-			files: [ '**/docs/**/*.js' ],
+			files: ['**/docs/**/*.js'],
 			rules: {
-				'ckeditor5-rules/ckeditor-imports': 'off'
-			}
-		}
-	]
+				'ckeditor5-rules/ckeditor-imports': 'off',
+			},
+		},
+	],
 };
